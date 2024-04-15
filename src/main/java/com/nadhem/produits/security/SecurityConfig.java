e4bf34336e6a278b48b551c15f981f033cd5c650
unknown <nadhemb@yahoo.com>
2024-04-15 15:14:25 +0100

    second commit

--- src/main/java/com/nadhem/produits/security/SecurityConfig.java
+++ src/main/java/com/nadhem/produits/security/SecurityConfig.java
@@ -1,15 +1,20 @@
 package com.nadhem.produits.security;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -30,9 +35,12 @@ public class SecurityConfig {
 	
 	
 
-	    @Bean
+	   /* @Bean
 	    public InMemoryUserDetailsManager userDetailsService() {
 	      PasswordEncoder passwordEncoder = passwordEncoder ();
+	      System.out.println("Password Encoded BCRYPT :******************** ");
+	  	  System.out.println(passwordEncoder.encode("123"));
+
 	    	
 	    	UserDetails admin = User
 	    			.withUsername("admin")
@@ -52,6 +60,53 @@ public class SecurityConfig {
 	         
 	        return new InMemoryUserDetailsManager(admin, userNadhem,user1);
 	    }
+	    */
+	 
+	    @Bean
+	    public UserDetailsService userDetailsService(DataSource dataSource) {
+	    	JdbcUserDetailsManager jdbcUserDetailsManager =new JdbcUserDetailsManager(dataSource);
+	    	
+	    	jdbcUserDetailsManager.setUsersByUsernameQuery("select username , password , enabled from user where username =?");
+	    	jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("SELECT u.username, r.role as authority   " +
+	    			"FROM user_role ur, user u , role r " +
+	    	        "WHERE u.user_id = ur.user_id AND ur.role_id = r.role_id AND u.username = ?");
+	    	
+	    	//jdbcUserDetailsManager.setRolePrefix("ROLE_");
+	    	
+	    	
+	    	return jdbcUserDetailsManager;
+	    }
+	    
+	    
+	
+	 
+	  
+	 /*   public void configureGlobal(AuthenticationManagerBuilder auth,DataSource dataSource) 
+	      throws Exception {
+	        /*auth.jdbcAuthentication()
+	          .dataSource(dataSource)
+	          .usersByUsernameQuery("select email,password,enabled "
+	            + "from bael_users "
+	            + "where email = ?")
+	          .authoritiesByUsernameQuery("select email,authority "
+	            + "from authorities "
+	            + "where email = ?");
+	    	   PasswordEncoder passwordEncoder = passwordEncoder ();
+	        auth.jdbcAuthentication()
+	    	.dataSource(dataSource)
+	    	.usersByUsernameQuery("select username , password , enabled from user where username =?")
+	    	.authoritiesByUsernameQuery(
+	    	"SELECT u.username, r.role " +
+	    	"FROM user_role ur, user u , role r " +
+	    	"WHERE u.user_id = ur.user_id AND ur.role_id = r.role_id AND u.username = ?")
+	    	.passwordEncoder(passwordEncoder);
+	                
+	        
+	    }*/
+	    
+	    
+	  
+
 	    
 	    @Bean
 	    public PasswordEncoder passwordEncoder () {
